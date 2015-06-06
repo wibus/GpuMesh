44c1457d508b4edae09ede8eae42e761779d588e
William Bussiere <william.bussiere@gmail.com>
2015-06-06 16:17:24 -0400

    Find Delaunay ball is way too slow. Must find a way to test less Tetrahedrons.

--- Mesh.h
+++ Mesh.h
@@ -1,6 +1,7 @@
 #ifndef GPUMESH_MESH
 #define GPUMESH_MESH
 
+#include <set>
 #include <list>
 #include <vector>
 #include <memory>
@@ -9,15 +10,17 @@
 #include <GLM/glm.hpp>
 
 
+struct Tetrahedron;
+
 struct Vertex
 {
-    Vertex() : p(), rank{-1, -1, -1} {}
+    Vertex() {}
     Vertex(const glm::dvec3 pos) :
         p(pos)
     {}
 
     glm::dvec3 p;
-    int rank[3];
+    std::set<std::shared_ptr<Tetrahedron>> tetra;
 };
 
 struct Triangle
@@ -102,24 +105,59 @@ struct Tetrahedron
     {
     }
 
+    inline int t0v0() const {return v[0];}
+    inline int t0v1() const {return v[1];}
+    inline int t0v2() const {return v[2];}
+    inline Triangle t0 () const
+    {
+        return Triangle(t0v0(), t0v1(), t0v2());
+    }
+
+    inline int t1v0() const {return v[0];}
+    inline int t1v1() const {return v[2];}
+    inline int t1v2() const {return v[3];}
+    inline Triangle t1 () const
+    {
+        return Triangle(t1v0(), t1v1(), t1v2());
+    }
+
+    inline int t2v0() const {return v[0];}
+    inline int t2v1() const {return v[3];}
+    inline int t2v2() const {return v[1];}
+    inline Triangle t2 () const
+    {
+        return Triangle(t2v0(), t2v1(), t2v2());
+    }
+
+    inline int t3v0() const {return v[3];}
+    inline int t3v1() const {return v[2];}
+    inline int t3v2() const {return v[1];}
+    inline Triangle t3 () const
+    {
+        return Triangle(t3v0(), t3v1(), t3v2());
+    }
+
     int v[4];
+
+    // Algo flag
+    bool flag;
+
+    // Data cache
     double circumRadius2;
     glm::dvec3 circumCenter;
+    glm::ivec3 cId;
 };
 
-
-struct KdNode
+struct GridCell
 {
-    std::shared_ptr<KdNode> left;
-    std::shared_ptr<KdNode> right;
-    std::list<Tetrahedron> tetra;
-    glm::dvec3 sepAxis;
-    int separator;
+    std::vector<int> vertId;
+    std::set<std::shared_ptr<Tetrahedron>> tetra;
 };
 
 class Mesh
 {
 public:
+
     inline unsigned int vertCount() const
     {
         return vert.size();
@@ -131,8 +169,8 @@ public:
     }
 
     void initialize(
-            std::vector<glm::dvec3>& vertices,
-            std::vector<Tetrahedron>& tetrahedras);
+            const std::vector<glm::dvec3>& vertices,
+            const std::vector<Tetrahedron>& tetrahedras);
 
     void compileArrayBuffers(
             std::vector<unsigned int>& indices,
@@ -141,26 +179,31 @@ public:
     void insertVertices(const std::vector<glm::dvec3>& vertices);
 
     std::vector<Vertex> vert;
-    std::list<Tetrahedron> tetra;
-    std::shared_ptr<KdNode> kdRoot;
+    std::list<std::shared_ptr<Tetrahedron>> tetra;
+
 
 private:
-    void computeCircumSphere(Tetrahedron& tetra);
-
     void insertVertex(int id);
+    void computeDataCache(Tetrahedron& tet);
 
-    std::shared_ptr<KdNode> buildKdTree(int height);
-    void buildSubKdTree(const std::shared_ptr<KdNode>& node,
-                        const std::vector<int> sorts[3],
-                        const glm::ivec3& sepAxis,
-                        int height);
+    void initializeGrid(int idStart, int idEnd);
+    void insertCell(const glm::ivec3& cId);
+    void insertVertexGrid(const glm::ivec3& cId, int vId);
+    std::shared_ptr<Tetrahedron> findBaseTetrahedron(const glm::ivec3& cId, int vId);
+    void findDelaunayBall(int vId, const std::shared_ptr<Tetrahedron>& base,
+                          std::set<Triangle>& ball);
+    bool isBase(int vId, const std::shared_ptr<Tetrahedron>& tet);
+    void insertTetrahedronGrid(const glm::ivec3& cId,
+                               const std::shared_ptr<Tetrahedron>& tet);
+    void removeTetrahedronGrid(const glm::ivec3& cId,
+                               const std::shared_ptr<Tetrahedron>& tet);
+    void collectTetrahedronsGrid();
 
-    void insertVertexKd(const std::shared_ptr<KdNode>& root, int id);
-    void insertTetrahedronKd(const std::shared_ptr<KdNode>& root,
-                             const Tetrahedron& tet);
 
-    void collectTetrahedronsInKd(const std::shared_ptr<KdNode>& root);
-    void printLoads(const std::shared_ptr<KdNode> root);
+    glm::ivec3 gridSize;
+    std::vector<std::vector<std::vector<GridCell>>> grid;
+    glm::dvec3 cMin;
+    glm::dvec3 cMax;
 };
 
 
