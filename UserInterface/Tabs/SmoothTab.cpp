b760f415eca2a25dce5a0448b2bbc036fd97157d
William Bussiere <william.bussiere@gmail.com>
2015-08-12 10:02:20 -0400

    Adding min quality value and algorithm's final quality mean values to the plot.

--- UserInterface/Tabs/SmoothTab.cpp
+++ UserInterface/Tabs/SmoothTab.cpp
@@ -181,80 +181,113 @@ void SmoothTab::displayOptimizationPlot(const OptimizationPlot& plot)
     double sceneWidth = 800;
     double sceneHeight = 600;
 
-    double maxX = 0.0;
+    double maxT = 0.0;
+    double minQ = 1.0;
+    double maxQ = 0.0;
+    map<string, QPen> pens;
     for(const auto& keyVal : plot.curves())
     {
+        const string& label = keyVal.first;
+
+        pens.insert(make_pair(
+            label,
+            QPen(QColor(
+                ((uchar) label[0]) % 3 * 75,
+                ((uchar) label[1]) % 3 * 75,
+                ((uchar) label[2]) % 3 * 75))
+        ));
+
         for(const auto& pass : keyVal.second)
         {
-            maxX = glm::max(maxX, pass.timeStamp);
+            maxT = glm::max(maxT, pass.timeStamp);
+            minQ = glm::min(minQ, pass.qualityMean);
+            maxQ = glm::max(maxQ, pass.qualityMean);
         }
     }
+    double marginQ = (maxQ - minQ) * 0.05;
+    double bottomQ = minQ - marginQ;
+    double topQ = maxQ + marginQ;
+    double scaleQ = 1.0 / (topQ - bottomQ);
 
+    // Min timestamp and min qualityMean
+    QGraphicsTextItem* minTimeText = _currentScene->addText("0s");
+    double minTimeTextLen = minTimeText->document()->size().width();
+    minTimeText->setPos(-minTimeTextLen/2.0, sceneHeight);
+
+    // Algo final quality mean
+    QGraphicsTextItem* minQualityText = _currentScene->addText(QString::number(minQ));
+    double minQualityTextWidth = minQualityText->document()->size().width();
+    double minQualityTextHeight = minQualityText->document()->size().height();
+    minQualityText->setPos(-minQualityTextWidth, sceneHeight * (1.0-marginQ*scaleQ) - minQualityTextHeight/2.0);
+
+
+    double labelHeight = 10.0;
     double legendLeft = sceneWidth - 200;
     double legendTop = sceneHeight - 20.0 * (plot.curves().size() + 2);
-    double labelHeight = 10.0;
     for(const auto& keyVal : plot.curves())
     {
         const string& label = keyVal.first;
         const OptimizationPassVect& samples = keyVal.second;
 
-        QPen pen(QColor(
-            ((uchar) label[0]) % 3 * 75,
-            ((uchar) label[1]) % 3 * 75,
-            ((uchar) label[2]) % 3 * 75));
-
         // Asymptotes
         double totalTime = samples.back().timeStamp;
-        double xAsymptote = sceneWidth * (totalTime / maxX);
+        double xAsymptote = sceneWidth * (totalTime / maxT);
         _currentScene->addLine(xAsymptote, 0, xAsymptote, sceneHeight, QPen(Qt::lightGray));
-        double yAsymptote = sceneHeight * (1.0 - samples.back().qualityMean);
+        double yAsymptote = sceneHeight * (topQ - samples.back().qualityMean) * scaleQ;
         _currentScene->addLine(0, yAsymptote, sceneWidth, yAsymptote, QPen(Qt::lightGray));
 
         // Algo total times
         QGraphicsTextItem* timeText = _currentScene->addText(QString::number(totalTime) + "s");
-        timeText->adjustSize();
-        timeText->setPos(xAsymptote - timeText->textWidth()/2.0, sceneHeight);
-        timeText->setDefaultTextColor(pen.color());
+        double timeTextLen = timeText->document()->size().width();
+        timeText->setPos(xAsymptote - timeTextLen/2.0, sceneHeight);
+        timeText->setDefaultTextColor(pens[label].color());
+
+        // Algo final quality mean
+        double finalQuality = samples.back().qualityMean;
+        QGraphicsTextItem* qualityText = _currentScene->addText(QString::number(finalQuality));
+        double qualityTextHeight = qualityText->document()->size().height();
+        qualityText->setPos(sceneWidth, sceneHeight * (topQ - finalQuality) * scaleQ - qualityTextHeight/2.0);
+        qualityText->setDefaultTextColor(pens[label].color());
 
         // Legend
         double gainValue = samples.back().qualityMean - samples.front().qualityMean;
         QString gainText = " (" + ((gainValue < 0.0 ? "-" : "+") + QString::number(gainValue)) + ")";
         QGraphicsTextItem* text = _currentScene->addText(label.c_str() + gainText);
         text->setPos(legendLeft + 10.0, legendTop + labelHeight);
-        text->setDefaultTextColor(pen.color());
+        text->setDefaultTextColor(pens[label].color());
         labelHeight += 20.0;
     }
     _currentScene->addRect(
         legendLeft, legendTop,
         180.0, 20.0 * (plot.curves().size()+1.3));
 
+
+    // Optimization curves
     for(const auto& keyVal : plot.curves())
     {
         const string& label = keyVal.first;
         const OptimizationPassVect& samples = keyVal.second;
-
-        QPen pen(QColor(
-            ((uchar) label[0]) % 3 * 75,
-            ((uchar) label[1]) % 3 * 75,
-            ((uchar) label[2]) % 3 * 75));
-
         for(size_t i=1; i < samples.size(); ++i)
         {
             const OptimizationPass& prevPass = samples[i-1];
             const OptimizationPass& currPass = samples[i];
             _currentScene->addLine(
-                sceneWidth * prevPass.timeStamp / maxX,
-                sceneHeight * (1.0 - prevPass.qualityMean),
-                sceneWidth * currPass.timeStamp / maxX,
-                sceneHeight * (1.0 - currPass.qualityMean),
-                pen);
+                sceneWidth * prevPass.timeStamp / maxT,
+                sceneHeight * (topQ - prevPass.qualityMean) * scaleQ,
+                sceneWidth * currPass.timeStamp / maxT,
+                sceneHeight * (topQ - currPass.qualityMean) * scaleQ,
+                pens[label]);
         }
     }
+
+    // Graphics borders
     _currentScene->addRect(0, 0, sceneWidth, sceneHeight);
 
 
-    QGraphicsTextItem* titleText = _currentScene->addText(plot.title().c_str());
-    titleText->setPos((sceneWidth - titleText->document()->size().width())/2.0, -30.0);
+    QFont titleFont;
+    titleFont.setPointSize(20);
+    QGraphicsTextItem* titleText = _currentScene->addText(plot.title().c_str(), titleFont);
+    titleText->setPos((sceneWidth - titleText->document()->size().width())/2.0, -50.0);
 
     _currentView->setScene(_currentScene);
     _currentView->setDragMode(QGraphicsView::ScrollHandDrag);
@@ -262,7 +295,7 @@ void SmoothTab::displayOptimizationPlot(const OptimizationPlot& plot)
         QPainter::Antialiasing |
         QPainter::SmoothPixmapTransform);
     _currentView->resize(
-        sceneWidth + 50.0,
-        sceneHeight + 60.0);
+        sceneWidth + 180.0,
+        sceneHeight + 100.0);
     _currentView->show();
 }
